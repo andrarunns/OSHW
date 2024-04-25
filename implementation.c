a80733904a2f2b23cd572c3fa138a5755222226b
GusBuzzz <grubio5@miners.utep.edu>
2024-04-24 22:14:20 -0600

    Cleaned up

--- implementation.c
+++ implementation.c
@@ -23,11 +23,8 @@ typedef unsigned int u_int;
 
 /* Definitions and type declarations */
 
-//#define ((size_t)255) ((size_t)255)
 
 typedef size_t __myfs_off_t;
-
-
 
 typedef struct {
   size_t size_left;
@@ -60,11 +57,7 @@ typedef struct {
 } file_t;
 
 typedef struct {
-  // Max number of children is given at the children array location -
-  // sizeof(size_t) divided by the sizeof(__myfs_off_t)
   size_t max_num_children;
-  // children is an offset to an array of offsets to folders and files. Children
-  // starts with '..' offsets
   __myfs_off_t children;
 } directory_t;
 
@@ -106,8 +99,6 @@ void free_file_info(void *fsptr, file_t *file);
 void remove_node(void *fsptr, directory_t *dict, node_t *node);
 void remove_data(void *fsptr, myfs_file_block_struct_t *block, size_t size);
 int add_data(void *fsptr, file_t *file, size_t size, int *errnoptr);
-
-/* START memory allocation implementations */
 
 void add_allocation_space(void *fsptr, list_node_t *LL, node_struct_t *alloc) {
     // Initialize variables
@@ -283,8 +274,7 @@ void *get_allocation(void *fsptr, list_node_t *LL, node_struct_t *org_pref,
     ptr = largest;
     // Check if the largest block can give everything that we are missing
     if (largest->size_left >= *size) {
-      // Check if we can make an node_struct_t object after getting size bytes
-      // from it
+      // Check if we can make an node_struct_t object after getting size bytes from it
       if (largest->size_left > *size + sizeof(node_struct_t)) {
         // Make the new node_struct_t object
         temp = ((void *)largest) + sizeof(size_t) + *size;  //
@@ -314,8 +304,6 @@ void *get_allocation(void *fsptr, list_node_t *LL, node_struct_t *org_pref,
       }
       *size = ((__myfs_off_t)0);
     }
-    // We couldn't got everything from the largest block so we get as much as we
-    // can from it
     else {
       // Update pointers
       if (before_largest == NULL) {
@@ -328,14 +316,10 @@ void *get_allocation(void *fsptr, list_node_t *LL, node_struct_t *org_pref,
       }
     }
   }
-
-  // We suppose to return the new block address if a new block outside the
-  // prefer one was needed
   if (ptr == NULL) {
     return NULL;
   }
-  return ((void *)ptr) + sizeof(size_t);  // ptr was set to the largest which is
-                                          // at the size_t header so we resize
+  return ((void *)ptr) + sizeof(size_t);  // ptr was set to the largest which is at the size_t header so we resize
 }
 
 /* If size is zero, return NULL. Otherwise, call get_allocation with size. */
@@ -430,8 +414,6 @@ void __free_impl(void *fsptr, void *ptr) {
 
 
 /* END memory allocation implementation */
-
-/* START of FUSE implementation */
 
 /* HELPER FUNCTIONS implementations */
 void *off_to_ptr(void *fsptr, __myfs_off_t offset) {
@@ -1044,30 +1026,8 @@ int add_data(void *fsptr, file_t *file, size_t size, int *errnoptr) {
 }
 /* End of helper functions */
 
-/* Implements an emulation of the stat system call on the filesystem of size
-   fssize pointed to by fsptr.
+/* START of FUSE implementation */
 
-   If path can be followed and describes a file or directory that exists and is
-   accessable, the access information is put into stbuf.
-
-   On success, 0 is returned. On failure, -1 is returned and the appropriate
-   error code is put into *errnoptr.
-
-   man 2 stat documents all possible error codes and gives more detail on what
-   fields of stbuf need to be filled in. Essentially, only the following fields
-   need to be supported:
-
-   st_uid      the value passed in argument
-   st_gid      the value passed in argument
-   st_mode     (as fixed values S_IFDIR | 0755 for directories,
-                                S_IFREG | 0755 for files)
-   st_nlink    (as many as there are subdirectories (not files) for directories
-                (including . and ..),
-                1 for files)
-   st_size     (supported only for files, where it is the real file size)
-   st_atim
-   st_mtim
-*/
 int __myfs_getattr_implem(void *fsptr, size_t fssize, int *errnoptr, uid_t uid,
                           gid_t gid, const char *path, struct stat *stbuf) {
   initilaize_check(fsptr, fssize);
@@ -1104,38 +1064,6 @@ int __myfs_getattr_implem(void *fsptr, size_t fssize, int *errnoptr, uid_t uid,
   return 0;
 }
 
-/* Implements an emulation of the readdir system call on the filesystem of size
-   fssize pointed to by fsptr.
-
-   If path can be followed and describes a directory that exists and is
-   accessable, the names of the subdirectories and files contained in that
-   directory are output into *namesptr. The . and .. directories must not be
-   included in that listing.
-
-   If it needs to output file and subdirectory names, the function starts by
-   allocating (with calloc) an array of pointers to characters of the right size
-   (n entries for n names). Sets *namesptr to that pointer. It then goes over
-   all entries in that array and allocates, for each of them an array of
-   characters of the right size (to hold the i-th name, together with the
-   appropriate '\0' terminator). It puts the pointer into that i-th array entry
-   and fills the allocated array of characters with the appropriate name. The
-   calling function will call free on each of the entries of *namesptr and on
-   *namesptr.
-
-   The function returns the number of names that have been put into namesptr.
-
-   If no name needs to be reported because the directory does not contain any
-   file or subdirectory besides . and .., 0 is returned and no allocation takes
-   place.
-
-   On failure, -1 is returned and the *errnoptr is set to the appropriate error
-   code.
-
-   The error codes are documented in man 2 readdir.
-
-   In the case memory allocation with malloc/calloc fails, failure is indicated
-   by returning -1 and setting *errnoptr to EINVAL.
-*/
 int __myfs_readdir_implem(void *fsptr, size_t fssize, int *errnoptr,
                           const char *path, char ***namesptr) {
   initilaize_check(fsptr, fssize);
@@ -1186,22 +1114,6 @@ int __myfs_readdir_implem(void *fsptr, size_t fssize, int *errnoptr,
   return ((int)(n_children - 1));
 }
 
-/* Implements an emulation of the mknod system call for regular files on the
-   filesystem of size fssize pointed to by fsptr.
-
-   This function is called only for the creation of regular files.
-
-   If a file gets created, it is of size zero and has default ownership and mode
-   bits.
-
-   The call creates the file indicated by path.
-
-   On success, 0 is returned.
-
-   On failure, -1 is returned and *errnoptr is set appropriately.
-
-   The error codes are documented in man 2 mknod.
-*/
 int __myfs_mknod_implem(void *fsptr, size_t fssize, int *errnoptr,
                         const char *path) {
   initilaize_check(fsptr, fssize);
@@ -1218,23 +1130,11 @@ int __myfs_mknod_implem(void *fsptr, size_t fssize, int *errnoptr,
   return 0;
 }
 
-/* Implements an emulation of the unlink system call for regular files on the
-   filesystem of size fssize pointed to by fsptr.
-
-   This function is called only for the deletion of regular files.
-
-   On success, 0 is returned.
-
-   On failure, -1 is returned and *errnoptr is set appropriately.
-
-   The error codes are documented in man 2 unlink.
-*/
 int __myfs_unlink_implem(void *fsptr, size_t fssize, int *errnoptr,
                          const char *path) {
   initilaize_check(fsptr, fssize);
 
-  // Call path_solver with a 1 because we want the directory node where the
-  // filename belongs to
+  // Call path_solver with a 1 because we want the directory node where the filename belongs to
   node_t *node = path_solver(fsptr, path, 1);
 
   // Check that the file parent exist
@@ -1256,8 +1156,7 @@ int __myfs_unlink_implem(void *fsptr, size_t fssize, int *errnoptr,
   unsigned long len;
   char *filename = get_last_token(path, &len);
 
-  // Check that the parent don't contain a node with the same name as the one we
-  // are about to create
+  // Check that the parent does not contain a node with the same name as the one we are about to create
   node_t *file_node = get_node(fsptr, dict, filename);
 
   if (file_node == NULL) {
@@ -1283,26 +1182,11 @@ int __myfs_unlink_implem(void *fsptr, size_t fssize, int *errnoptr,
   return 0;
 }
 
-/* Implements an emulation of the rmdir system call on the filesystem of size
-   fssize pointed to by fsptr.
-
-   The call deletes the directory indicated by path.
-
-   On success, 0 is returned.
-
-   On failure, -1 is returned and *errnoptr is set appropriately.
-
-   The function call must fail when the directory indicated by path is not empty
-   (if there are files or subdirectories other than . and ..).
-
-   The error codes are documented in man 2 rmdir.
-*/
 int __myfs_rmdir_implem(void *fsptr, size_t fssize, int *errnoptr,
                         const char *path) {
   initilaize_check(fsptr, fssize);
 
-  // We can access the parent directory as the first children of the return
-  // directory
+  // We can access the parent directory as the first children of the return directory
   node_t *node = path_solver(fsptr, path, 0);
 
   // Check that the node exist
@@ -1335,17 +1219,6 @@ int __myfs_rmdir_implem(void *fsptr, size_t fssize, int *errnoptr,
   return 0;
 }
 
-/* Implements an emulation of the mkdir system call on the filesystem of size
-   fssize pointed to by fsptr.
-
-   The call creates the directory indicated by path.
-
-   On success, 0 is returned.
-
-   On failure, -1 is returned and *errnoptr is set appropriately.
-
-   The error codes are documented in man 2 mkdir.
-*/
 int __myfs_mkdir_implem(void *fsptr, size_t fssize, int *errnoptr,
                         const char *path) {
   initilaize_check(fsptr, fssize);
@@ -1353,8 +1226,6 @@ int __myfs_mkdir_implem(void *fsptr, size_t fssize, int *errnoptr,
   // Make a directory, 0 because it is not a file
   node_t *node = make_inode(fsptr, path, errnoptr, 0);
 
-  // Check if the node was successfully created, if it wasn't the errnoptr was
-  // already set so we just return failure with -1
   if (node == NULL) {
     return -1;
   }
@@ -1362,21 +1233,6 @@ int __myfs_mkdir_implem(void *fsptr, size_t fssize, int *errnoptr,
   return 0;
 }
 
-/* Implements an emulation of the rename system call on the filesystem of size
-   fssize pointed to by fsptr.
-
-   The call moves the file or directory indicated by from to to.
-
-   On success, 0 is returned.
-
-   On failure, -1 is returned and *errnoptr is set appropriately.
-
-   Caution: the function does more than what is hinted to by its name. In cases
-   the from and to paths differ, the file is moved out of the from path and
-   added to the to path.
-
-   The error codes are documented in man 2 rename.
-*/
 int __myfs_rename_implem(void *fsptr, size_t fssize, int *errnoptr,
                          const char *from, const char *to) {
   initilaize_check(fsptr, fssize);
@@ -1385,20 +1241,6 @@ int __myfs_rename_implem(void *fsptr, size_t fssize, int *errnoptr,
   return -1;
 }
 
-/* Implements an emulation of the truncate system call on the filesystem of size
-   fssize pointed to by fsptr.
-
-   The call changes the size of the file indicated by path to offset bytes.
-
-   When the file becomes smaller due to the call, the extending bytes are
-   removed. When it becomes larger, zeros are appended.
-
-   On success, 0 is returned.
-
-   On failure, -1 is returned and *errnoptr is set appropriately.
-
-   The error codes are documented in man 2 truncate.
-*/
 int __myfs_truncate_implem(void *fsptr, size_t fssize, int *errnoptr,
                            const char *path, off_t offset) {
   initilaize_check(fsptr, fssize);
@@ -1464,29 +1306,6 @@ int __myfs_truncate_implem(void *fsptr, size_t fssize, int *errnoptr,
   return 0;
 }
 
-/* Implements an emulation of the open system call on the filesystem  of size
-   fssize pointed to by fsptr, without actually performing the opening of the
-   file (no file descriptor is returned).
-
-   The call just checks if the file (or directory) indicated by path can be
-   accessed, i.e. if the path can be followed to an existing object for which
-   the access rights are granted.
-
-   On success, 0 is returned.
-
-   On failure, -1 is returned and *errnoptr is set appropriately.
-
-   The two only interesting error codes are
-
-   * EFAULT: the filesystem is in a bad state, we can't do anything
-
-   * ENOENT: the file that we are supposed to open doesn't exist (or a subpath).
-
-   It is possible to restrict ourselves to only these two error conditions. It
-   is also possible to implement more detailed error condition answers.
-
-   The error codes are documented in man 2 open.
-*/
 int __myfs_open_implem(void *fsptr, size_t fssize, int *errnoptr,
                        const char *path) {
   initilaize_check(fsptr, fssize);
@@ -1504,20 +1323,6 @@ int __myfs_open_implem(void *fsptr, size_t fssize, int *errnoptr,
   return node->is_file ? 0 : -1;
 }
 
-/* Implements an emulation of the read system call on the filesystem of size
-   fssize pointed to by fsptr.
-
-   The call copies up to size bytes from the file indicated by path into the
-   buffer, starting to read at offset. See the man page for read for the details
-   when offset is beyond the end of the file etc.
-
-   On success, the appropriate number of bytes read into the buffer is returned.
-   The value zero is returned on an end-of-file condition.
-
-   On failure, -1 is returned and *errnoptr is set appropriately.
-
-   The error codes are documented in man 2 read.
-*/
 int __myfs_read_implem(void *fsptr, size_t fssize, int *errnoptr,
                        const char *path, char *buf, size_t size, off_t offset) {
   initilaize_check(fsptr, fssize);
@@ -1528,8 +1333,7 @@ int __myfs_read_implem(void *fsptr, size_t fssize, int *errnoptr,
     return -1;
   }
 
-  // off_t is signed but we already know that it is positive so we change it to
-  // size_t
+  // off_t is signed but we already know that it is positive so we change it to size_t
   size_t size_left = ((size_t)offset);
 
   // Getting file node
@@ -1547,8 +1351,7 @@ int __myfs_read_implem(void *fsptr, size_t fssize, int *errnoptr,
     return -1;
   }
 
-  // Check that the file have more bytes than the size_left so we don't have to
-  // iterate it
+  // Check that the file have more bytes than the size_left so we don't have to iterate it
   file_t *file = &node->type.file;
 
   // If we are trying to read outside the file size
@@ -1575,8 +1378,7 @@ int __myfs_read_implem(void *fsptr, size_t fssize, int *errnoptr,
     }
   }
 
-  // We have the index to where to start reading so we start populating the
-  // buffer
+  // We have the index to where to start reading so we start populating the buffer
   size_t tot_read_bytes = 0;
   size_t read_n_bytes =
       size > (block->allocated - index) ? (block->allocated - index) : size;
@@ -1600,20 +1402,6 @@ int __myfs_read_implem(void *fsptr, size_t fssize, int *errnoptr,
   return ((int)tot_read_bytes);
 }
 
-/* Implements an emulation of the write system call on the filesystem of size
-   fssize pointed to by fsptr.
-
-   The call copies up to size bytes to the file indicated by path into the
-   buffer, starting to write at offset. See the man page for write for the
-   details when offset is beyond the end of the file etc.
-
-   On success, the appropriate number of bytes written into the file is
-   returned. The value zero is returned on an end-of-file condition.
-
-   On failure, -1 is returned and *errnoptr is set appropriately.
-
-   The error codes are documented in man 2 write.
-*/
 int __myfs_write_implem(void *fsptr, size_t fssize, int *errnoptr,
                         const char *path, const char *buf, size_t size,
                         off_t offset) {
@@ -1640,8 +1428,7 @@ int __myfs_write_implem(void *fsptr, size_t fssize, int *errnoptr,
     return -1;
   }
 
-  // Check that the file have more bytes than the size_left so we don't have to
-  // iterate it
+  // Check that the file have more bytes than the size_left so we don't have to iterate it
   file_t *file = &node->type.file;
   if (file->total_size < size_left) {
     *errnoptr = EFBIG;
@@ -1678,8 +1465,7 @@ int __myfs_write_implem(void *fsptr, size_t fssize, int *errnoptr,
 
   memcpy(copy, buf, size);
   copy[size] = '\0';
-  // Start writing at index on the data block saving what is currently on it to
-  // later be push forward
+  // Start writing at index on the data block saving what is currently on it to later be push forward
   while ((buf[buf_idx] != '\0') && (((void *)block) != fsptr)) {
     while (buf_idx != size) {
       // Swap characters from buffer to data block
@@ -1724,7 +1510,6 @@ end:
   return ((int)size);
 }
 
-
 int __myfs_utimens_implem(void *fsptr, size_t fssize, int *errnoptr,
                           const char *path, const struct timespec ts[2]) {
     // Check if fsptr is initialized
@@ -1747,7 +1532,6 @@ int __myfs_utimens_implem(void *fsptr, size_t fssize, int *errnoptr,
     return 0; 
 }
 
-
 int __myfs_statfs_implem(void *fsptr, size_t fssize, int *errnoptr,
                          struct statvfs *stbuf) {
       // Check if stbuf is NULL
@@ -1755,8 +1539,6 @@ int __myfs_statfs_implem(void *fsptr, size_t fssize, int *errnoptr,
             *errnoptr = EFAULT;
             return -1;
       }
-
-  
       stbuf->f_bsize = BLOCK_SIZE;
 
       //number of blocks in the filesystem
